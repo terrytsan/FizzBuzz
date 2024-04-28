554ba1aa1b858d5d5a9f7e8a4721f1c1b23c88f3
Terry <terrytsan@hotmail.com>
2024-04-28 11:04:57 +0100

    Added Generator failing test

--- FizzBuzz.Tests/FizzBuzzGeneratorTests.cs
+++ FizzBuzz.Tests/FizzBuzzGeneratorTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using Moq;
+using Xunit;
 
 namespace FizzBuzz.Tests;
 
@@ -14,5 +15,18 @@ public class FizzBuzzGeneratorTests
 		var results = generator.GenerateResults(lowerBound, upperBound);
 
 		Assert.Equal(expected, results.Count);
+	}
+
+	[Theory]
+	[InlineData(1, 100, 100)]
+	[InlineData(-100, 100, 201)]
+	public void GenerateResults_CallsProcessorMethodToRetrieveResults(int lowerBound, int upperBound, int expectedCalls)
+	{
+		var processor = new Mock<IFizzBuzzProcessor>([]);
+		IFizzBuzzGenerator generator = new FizzBuzzGenerator(processor.Object);
+
+		_ = generator.GenerateResults(lowerBound, upperBound);
+
+		processor.Verify(p => p.GenerateResult(It.IsAny<int>()), Times.Exactly(expectedCalls));
 	}
 }
