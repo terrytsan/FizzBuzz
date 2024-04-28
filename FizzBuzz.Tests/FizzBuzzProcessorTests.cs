3b0fb46593ec8e363478ac4da34a2dd70c040b82
Terry <terrytsan@hotmail.com>
2024-04-28 11:30:34 +0100

    Added test for processor to use the highest possible rule

--- FizzBuzz.Tests/FizzBuzzProcessorTests.cs
+++ FizzBuzz.Tests/FizzBuzzProcessorTests.cs
@@ -37,4 +37,32 @@ public class FizzBuzzProcessorTests
 
 		Assert.Equal(expected, result);
 	}
+
+	[Theory]
+	[InlineData(1, "1")]
+	[InlineData(3, "factor of three")]
+	[InlineData(5, "factor of five")]
+	[InlineData(10, "factor of five")]
+	[InlineData(20, "factor of five")]
+	[InlineData(21, "factor of seven")]
+	public void GenerateResult_AppliesRulesInOrderOfDivisorDescending(int input, string expected)
+	{
+		var firstRule = new Mock<Rule>();
+		firstRule.Setup(r => r.Divisor).Returns(5);
+		firstRule.Setup(r => r.IsApplicable(It.Is<int>(x => x % 5 == 0))).Returns(true);
+		firstRule.Setup(r => r.Process(It.IsAny<int>())).Returns("factor of five");
+		var secondRule = new Mock<Rule>();
+		secondRule.Setup(r => r.Divisor).Returns(3);
+		secondRule.Setup(r => r.IsApplicable(It.Is<int>(x => x % 3 == 0))).Returns(true);
+		secondRule.Setup(r => r.Process(It.IsAny<int>())).Returns("factor of three");
+		var thirdRule = new Mock<Rule>();
+		thirdRule.Setup(r => r.Divisor).Returns(7);
+		thirdRule.Setup(r => r.IsApplicable(It.Is<int>(x => x % 7 == 0))).Returns(true);
+		thirdRule.Setup(r => r.Process(It.IsAny<int>())).Returns("factor of seven");
+		var processor = new FizzBuzzProcessor([firstRule.Object, secondRule.Object, thirdRule.Object]);
+
+		var result = processor.GenerateResult(input);
+
+		Assert.Equal(expected, result);
+	}
 }
