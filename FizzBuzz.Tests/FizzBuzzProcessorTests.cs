c80fed86a8179a76ed54e1407f776bc9b14fe38c
Terry <terrytsan@hotmail.com>
2024-04-28 11:17:54 +0100

    Added test for processor involving single rule

--- FizzBuzz.Tests/FizzBuzzProcessorTests.cs
+++ FizzBuzz.Tests/FizzBuzzProcessorTests.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using FizzBuzz.Rules;
+using Moq;
+using Xunit;
 
 namespace FizzBuzz.Tests;
 
@@ -11,6 +13,25 @@ public class FizzBuzzProcessorTests
 	public void GenerateResult_ShouldReturnInputAsStringIfThereAreNoRules(int input, string expected)
 	{
 		var processor = new FizzBuzzProcessor([]);
+
+		var result = processor.GenerateResult(input);
+
+		Assert.Equal(expected, result);
+	}
+
+	[Theory]
+	[InlineData(1, "1")]
+	[InlineData(3, "3")]
+	[InlineData(5, "factor of five")]
+	[InlineData(10, "factor of five")]
+	[InlineData(20, "factor of five")]
+	[InlineData(21, "21")]
+	public void GenerateResult_AppliesSingleRuleCorrectly(int input, string expected)
+	{
+		var firstRule = new Mock<Rule>();
+		firstRule.Setup(r => r.IsApplicable(It.Is<int>(x => x % 5 == 0))).Returns(true);
+		firstRule.Setup(r => r.Process(It.IsAny<int>())).Returns("factor of five");
+		var processor = new FizzBuzzProcessor([firstRule.Object]);
 
 		var result = processor.GenerateResult(input);
 
